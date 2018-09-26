153c67117b4d57d72bf6db175f6b5db8aca6b4c0
Partast1 <43130360+Partast1@users.noreply.github.com>
2018-09-26 14:30:53 +0200

    a

--- TS4-Mod-Administration/ModsIndexer.cs
+++ TS4-Mod-Administration/ModsIndexer.cs
@@ -74,7 +74,7 @@ namespace TS4_Mod_Administration
 				this.trayModsFilesList = value;
 			}
 		}
-
+        // Anders har en kæmpe tissemand
 		public List<FileInfo> Ts4ScriptModsFilesList
 		{
 			get
