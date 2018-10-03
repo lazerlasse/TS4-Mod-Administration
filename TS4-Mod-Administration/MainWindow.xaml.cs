26233246f5ee8a96a47fb2b1e61247f375cded43
lazerlasse <31047935+lazerlasse@users.noreply.github.com>
2018-10-03 10:01:11 +0200

    Functions for uncompressing in MainWindow. done and testet. (Exeption are not testet yet!!!)

--- TS4-Mod-Administration/MainWindow.xaml.cs
+++ TS4-Mod-Administration/MainWindow.xaml.cs
@@ -9,14 +9,20 @@ namespace TS4_Mod_Administration
 	/// </summary>
 	public partial class MainWindow : Window
 	{
+		#region Attributes and Main Section
+
+		// Attributes...
 		private string browsePath;
 
+		// Initialize main window...
 		public MainWindow()
 		{
 			InitializeComponent();
 			ConflictSourcePatch.Text = SimsFolder.ModsFolderPath;
 		}
-         
+
+		#endregion Attributes and Main Section
+
 		#region Import Mods Section
 
 		// Start importing mods button click event...
@@ -48,7 +54,51 @@ namespace TS4_Mod_Administration
 		// Index and handle archive files before import...
 		private void HandleArchiveFiles()
 		{
+			// Index files to uncompress...
+			UncompressArchive uncompress = new UncompressArchive();
+			uncompress.IndexFilesToUncompress(browsePath);
 
+			// Check if files are added...
+			if (uncompress.FilesToUncompressCounter != 0)
+			{
+				// Show indexed files in DataGrid...
+				foreach (FileInfo file in uncompress.FilesToUncompress)
+				{
+					// Create DataGrid output...
+					UpdateImportDataGridView(new ProcessViewOutput
+					{
+						Package_Name = file.Name.Split('.')[0],
+						Package_Type = file.Extension,
+						Package_CreatedDate = file.CreationTime,
+						Package_EditedDate = file.LastWriteTime
+					});
+				}
+
+				// Show messagebox and get answer...
+				MessageBoxResult result = MessageBox.Show(
+						"Der blev fundet "
+						+ uncompress.FilesToUncompressCounter
+						+ " filer som kan udpakkes!!\nØnsker du at udpakke disse filer?",
+						"Der blev fundet filer som kan udpakkes!",
+						MessageBoxButton.YesNo);
+
+				// If user answered yes unpack files...
+				if (result == MessageBoxResult.Yes)
+				{
+					try
+					{
+						// Uncompress the files...
+						uncompress.UncompressArchiveFiles();
+					}
+					catch (Exception ex)
+					{
+						MessageBox.Show("Fejlmeddelelse " + ex.Message, "Der opstod en fejl!", MessageBoxButton.OK, MessageBoxImage.Error);
+					}
+
+					// Confirm unpacking succeded...
+					MessageBox.Show("Filerne blev udpakket med succes.", "Færdig", MessageBoxButton.OK, MessageBoxImage.Information);
+				}
+			}
 		}
 
 		#endregion Import Mods Section
