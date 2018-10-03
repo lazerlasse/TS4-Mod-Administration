671c746bbaaa5d417743e28612247bda5c18e3ef
lazerlasse <31047935+lazerlasse@users.noreply.github.com>
2018-10-03 15:45:11 +0200

    New class for progress created.

---
+++ TS4-Mod-Administration/TS4ProgressReporter.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TS4_Mod_Administration
+{
+	public class TS4ProgressReporter
+	{
+		public int ProgressPercentage { get; set; } = 0;
+	}
+}
